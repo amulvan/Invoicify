2099794235d00be3c4c67e9c8335d59c54c7f36c
FZ3BP1@ALLYIPOC.COM <FZ3BP1@ALLYIPOC.COM>
2018-06-21 15:27:59 -0400

    Controllers

--- src/main/java/com/el/ally/invoicify/controllers/BillingRecordController.java
+++ src/main/java/com/el/ally/invoicify/controllers/BillingRecordController.java
@@ -2,33 +2,46 @@ package com.el.ally.invoicify.controllers;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.el.ally.invoicify.Repositories.BillingRecordRepository;
 import com.el.ally.invoicify.models.BillingRecord;
 
-@Controller
-@RequestMapping("/")
+@RestController
+@RequestMapping("/controllers/billing-record")
 public class BillingRecordController {
+	
+	@Autowired
+	private BillingRecordRepository billingRepository;
 
 	public BillingRecordController(BillingRecordRepository billingRecordRepo) {
 		// TODO Auto-generated constructor stub
 	}
 
-	public List<BillingRecord> getAll() {
-		// TODO Auto-generated method stub
-		return null;
+	@GetMapping("/")
+	public List<BillingRecord> getAll() 
+	{
+		List<BillingRecord> BillingRecord = billingRepository.findAll();
+		return BillingRecord;
 	}
 
+	@PostMapping("/")
 	public BillingRecord create(BillingRecord billingRecord) {
-		// TODO Auto-generated method stub
-		return null;
+		BillingRecord newBillingRecord = billingRepository.save(billingRecord);
+		return newBillingRecord;
 	}
 
-	public BillingRecord getOne(int i) {
-		// TODO Auto-generated method stub
-		return null;
+	@GetMapping("/{i}")
+	public BillingRecord getOne(@PathVariable int i) {
+		BillingRecord billingRecord = (BillingRecord) billingRepository.findOne(i);
+		return billingRecord; 
+		
 	}
 
 
---
+++ src/main/java/com/el/ally/invoicify/controllers/CompanyController.java
@@ -0,0 +1,12 @@
+package com.el.ally.invoicify.controllers;
+
+public class CompanyController {
+
+	public CompanyController() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	@RestController
+	@RequestMapping
+
+}
