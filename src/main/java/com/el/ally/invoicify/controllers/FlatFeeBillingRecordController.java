91cffe253b1e8d6608f3a4f4e096756828f82e6d
Andrew Mulvaney <panthersontop@yahoo.com>
2018-06-21 15:07:43 -0400

    FlatFeeBillingRecordController

--- src/main/java/com/el/ally/invoicify/controllers/FlatFeeBillingRecordController.java
+++ src/main/java/com/el/ally/invoicify/controllers/FlatFeeBillingRecordController.java
@@ -1,12 +1,31 @@
 package com.el.ally.invoicify.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.el.ally.invoicify.Repositories.BillingRecordRepository;
 import com.el.ally.invoicify.models.FlatFeeBillingRecord;
 
+@Controller
+@RequestMapping
 public class FlatFeeBillingRecordController {
+	
+	@Autowired
+	private BillingRecordRepository billingRecordRepository;
+	
+@PostMapping("")
+public FlatFeeBillingRecord create (@RequestBody FlatFeeBillingRecord flatFee) {
+		FlatFeeBillingRecord newRecord = billingRecordRepository.save(flatFee);
+		 return newRecord;
+	    }
 	
 	public FlatFeeBillingRecord create() {
 		// TODO Auto-generated method stub
 		return null;
 	}
-
+	
+	
 }
