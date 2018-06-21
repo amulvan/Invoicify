78b91903808609ed8e0b49ecacc0db4194859c6c
tammy4117 <TZPJC0@ALLYIPOC.COM>
2018-06-21 13:53:58 -0400

    add file

--- src/main/java/com/el/ally/invoicify/controllers/RateBasedBillingRecordController.java
+++ src/main/java/com/el/ally/invoicify/controllers/RateBasedBillingRecordController.java
@@ -1,5 +1,12 @@
 package com.el.ally.invoicify.controllers;
 
+import com.el.ally.invoicify.models.RateBasedBillingRecord;
+
 public class RateBasedBillingRecordController {
+
+	public RateBasedBillingRecord create() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
