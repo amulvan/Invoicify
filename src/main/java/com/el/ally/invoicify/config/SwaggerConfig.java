7bf0b864f7be50f4696fdb65d310f69d30f90fb3
FZ3BP1@ALLYIPOC.COM <FZ3BP1@ALLYIPOC.COM>
2018-06-22 13:52:44 -0400

    SwaggerConfig2

--- src/main/java/com/el/ally/invoicify/config/SwaggerConfig.java
+++ src/main/java/com/el/ally/invoicify/config/SwaggerConfig.java
@@ -15,7 +15,7 @@ public class SwaggerConfig {
     public Docket productApi() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
-                .apis(RequestHandlerSelectors.basePackage("«com.el.ally.invoicify"))
+                .apis(RequestHandlerSelectors.basePackage("com.el.ally.invoicify"))
                 .build();
 
     }
