0a0223b56e5170b97cb80551184f18dcb16176bf
tammy4117 <TZPJC0@ALLYIPOC.COM>
2018-06-22 15:51:46 -0400

    security config

--- src/main/java/com/el/ally/invoicify/configuration/SecurityConfiguration.java
+++ src/main/java/com/el/ally/invoicify/configuration/SecurityConfiguration.java
@@ -1,6 +1,7 @@
 package com.el.ally.invoicify.configuration;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -12,6 +13,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 
 import com.el.ally.invoicify.services.AppUserDetailsService;
 
+@Configuration
 @EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
